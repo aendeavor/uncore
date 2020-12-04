281bba6aac0bb78b9e3b5f8f8aade2a680df5910
Georg Lauterbach <44545919+aendeavor@users.noreply.github.com>
2020-12-04 23:11:22 +0100

    adjusting workflows & adding linting depencies

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 : '
-# ? version       v0.1.0 RC1 ALPHA1 UNSTABLE
+# ? version       v0.1.0 RC1 PRODUCTION STABLE
 # ? executed by   just
 # ? task          handles init jobs
 '
@@ -19,12 +19,22 @@ function __install_deps
 {
   __log_info "installing dependencies"
 
-  rustup override set nightly
+  rustup override set nightly-2020-11-09
   rustup component add llvm-tools-preview
 
   cargo install cargo-xbuild bootimage
 
   sudo apt-get install qemu-system-x86
+
+  local ECLINT_VERSION='2.2.0'
+  local SHELLCHECK_VERSION='0.7.0-2build2'
+
+  sudo apt-get -qq --fix-missing update
+  sudo apt-get -qq -y --no-install-recommends install shellcheck="${SHELLCHECK_VERSION}" wget
+
+  cd /tmp
+  wget -qq --no-check-certificate "https://github.com/editorconfig-checker/editorconfig-checker/releases/download/${ECLINT_VERSION}/ec-linux-amd64.tar.gz"
+  tar xf ec-linux-amd64.tar.gz && chmod +x bin/ec-linux-amd64 && sudo mv bin/ec-linux-amd64 /usr/bin/eclint
 }
 
 function _main
